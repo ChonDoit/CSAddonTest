f78ad40631dfff03d9b637f7275d5e6924c2b983
ChonDoit <thphantomblog@gmail.com>
2026-03-24 16:19:47 -0300

    Regulary update

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,65 +38,69 @@ fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extens
 
 fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
 
+val categories = listOf("Anime", "Cartoons", "Doramas", "IPTV", "Streaming", "Torrents")
+
 subprojects {
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/ChonDoit/CSAddonTest")
-        authors = listOf("ChonDoit")
-    }
-
-    android {
-        namespace = "com.chondoit"
-
-        defaultConfig {
-            minSdk = 21
-            compileSdkVersion(35)
-            targetSdk = 35
-
+    if (project.name !in categories) {
+        apply(plugin = "com.android.library")
+        apply(plugin = "kotlin-android")
+        apply(plugin = "com.lagradost.cloudstream3.gradle")
+    
+        cloudstream {
+            setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/ChonDoit/CSAddonTest")
+            authors = listOf("ChonDoit")
         }
-
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-
-
-        tasks.withType<KotlinJvmCompile> {
-            compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8)
-                freeCompilerArgs.addAll(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions",
-                    "-Xno-receiver-assertions"
-                )
+    
+        android {
+            namespace = "com.chondoit"
+    
+            defaultConfig {
+                minSdk = 21
+                compileSdkVersion(35)
+                targetSdk = 35
+    
+            }
+    
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_1_8
+                targetCompatibility = JavaVersion.VERSION_1_8
+            }
+    
+    
+            tasks.withType<KotlinJvmCompile> {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_1_8)
+                    freeCompilerArgs.addAll(
+                        "-Xno-call-assertions",
+                        "-Xno-param-assertions",
+                        "-Xno-receiver-assertions"
+                    )
+                }
             }
         }
-    }
-
-    dependencies {
-        val implementation by configurations
-        val cloudstream by configurations
-        cloudstream("com.lagradost:cloudstream3:pre-release")
-
-        // Other dependencies
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.16")
-        implementation("org.jsoup:jsoup:1.22.1")
-        implementation("androidx.annotation:annotation:1.9.1")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
-        implementation("org.mozilla:rhino:1.9.0")
-        implementation("me.xdrop:fuzzywuzzy:1.4.0")
-        implementation("com.google.code.gson:gson:2.13.2")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
-        implementation("com.github.vidstige:jadb:v1.2.1")
-        implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
-        implementation("com.squareup.okhttp3:okhttp:4.12.0")
-        implementation("androidx.annotation:annotation:1.9.1")
+    
+        dependencies {
+            val implementation by configurations
+            val cloudstream by configurations
+            cloudstream("com.lagradost:cloudstream3:pre-release")
+    
+            // Other dependencies
+            implementation(kotlin("stdlib"))
+            implementation("com.github.Blatzar:NiceHttp:0.4.16")
+            implementation("org.jsoup:jsoup:1.22.1")
+            implementation("androidx.annotation:annotation:1.9.1")
+            implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.20.1")
+            implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+            implementation("org.mozilla:rhino:1.9.0")
+            implementation("me.xdrop:fuzzywuzzy:1.4.0")
+            implementation("com.google.code.gson:gson:2.13.2")
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+            implementation("com.github.vidstige:jadb:v1.2.1")
+            implementation("org.bouncycastle:bcpkix-jdk15on:1.70")
+            implementation("com.squareup.okhttp3:okhttp:4.12.0")
+            implementation("androidx.annotation:annotation:1.9.1")
+        }
     }
 }
 
