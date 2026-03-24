ecdc8acaa93ea9ec80ba809a7f6588e2dff60188
ChonDoit <thphantomblog@gmail.com>
2026-03-24 15:50:01 -0300

    Regulary update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,12 +14,14 @@ categories.forEach { category ->
     
     if (categoryDir.exists() && categoryDir.isDirectory) {
         categoryDir.listFiles()
-            ?.filter { it.isDirectory && File(it, "build.gradle.kts").exists() }
+            ?.filter { provider -> 
+                provider.isDirectory && 
+                provider.name != category &&
+                File(provider, "build.gradle.kts").exists() 
+            }
             ?.forEach { provider ->
-                
                 val projectPath = ":${category}:${provider.name}"
                 include(projectPath)
-                
                 project(projectPath).projectDir = provider
             }
     }
