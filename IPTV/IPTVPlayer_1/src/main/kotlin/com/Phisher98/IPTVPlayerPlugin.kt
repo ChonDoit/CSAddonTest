23692750c57c453c14d625ebc324545e8afa7f65
ChonDoit <thphantomblog@gmail.com>
2026-03-24 13:52:17 -0300

    Regulary update

--- IPTV/IPTVPlayer_1/src/main/kotlin/com/Phisher98/IPTVPlayerPlugin.kt
+++ IPTV/IPTVPlayer_1/src/main/kotlin/com/Phisher98/IPTVPlayerPlugin.kt
@@ -4,7 +4,7 @@ import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
-class IPTVPlayerPlugin: BasePlugin() {
+class IPTVPlayer_1Plugin: BasePlugin() {
     override fun load() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(IPTVPlayer())
--- Streaming/Pelisplushd_1/src/main/kotlin/com/pelisplushd/PelisplushdProvider.kt
+++ Streaming/Pelisplushd_1/src/main/kotlin/com/pelisplushd/PelisplushdProvider.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class Animeav1Provider: Plugin() {
+class PelisplushdProvider: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(Pelisplushd())
